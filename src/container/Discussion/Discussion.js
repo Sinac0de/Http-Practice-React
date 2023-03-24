7585904deeeaa12bc11da882c1b603f8eee7db0e
Sina Moradian <sinacsharp@gmail.com>
2023-03-25 02:01:22 +0330

    Initialize project

---
+++ src/container/Discussion/Discussion.js
@@ -0,0 +1,11 @@
+const Discussion = () => {
+  return (
+    <>
+      <section>comment</section>
+      <section>comment Details</section>
+      <section>add comment</section>
+    </>
+  );
+};
+
+export default Discussion;
