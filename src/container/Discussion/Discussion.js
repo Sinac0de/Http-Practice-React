f485e58eb6115e6c088b314b76e3d74b9e7e44fb
Sina Moradian <sinacsharp@gmail.com>
2023-03-25 23:47:34 +0330

    Add Get comments through Api

--- src/container/Discussion/Discussion.js
+++ src/container/Discussion/Discussion.js
@@ -1,16 +1,38 @@
+import { useEffect, useState } from "react";
 import Comment from "../../components/Comment/Comment";
 import FullComment from "../../components/FullComment/FullComment";
 import NewComment from "../../components/NewComment/NewComment";
+import axios from "axios";
 import "./discussion.css";
 
 const Discussion = () => {
+  const [comments, setComments] = useState(null);
+
+  useEffect(() => {
+    const getComments = async () => {
+      try {
+        const { data } = await axios.get(
+          "https://jsonplaceholder.typicode.com/comments"
+        );
+        setComments(data.slice(0, 4));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getComments();
+  }, []);
+
   return (
     <>
       <section>
-        <Comment />
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments ? (
+          comments.map((c) => (
+            <Comment key={c.id} name={c.name} email={c.email} />
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
       </section>
       <section>
         <FullComment />
