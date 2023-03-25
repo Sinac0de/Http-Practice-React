9819656d72b9aa393c136514ca93ac85e557900a
Sina Moradian <sinacsharp@gmail.com>
2023-03-25 04:29:19 +0330

    Add basic structure

--- src/container/Discussion/Discussion.js
+++ src/container/Discussion/Discussion.js
@@ -1,9 +1,23 @@
+import Comment from "../../components/Comment/Comment";
+import FullComment from "../../components/FullComment/FullComment";
+import NewComment from "../../components/NewComment/NewComment";
+import "./discussion.css";
+
 const Discussion = () => {
   return (
     <>
-      <section>comment</section>
-      <section>comment Details</section>
-      <section>add comment</section>
+      <section>
+        <Comment />
+        <Comment />
+        <Comment />
+        <Comment />
+      </section>
+      <section>
+        <FullComment />
+      </section>
+      <section>
+        <NewComment />
+      </section>
     </>
   );
 };
