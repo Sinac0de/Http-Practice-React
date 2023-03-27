964368c18806edd1e77b05d6d461259c0a37a21f
Sina Moradian <sinacsharp@gmail.com>
2023-03-27 05:20:58 +0330

    Add toastify notification

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Discussion from "./container/Discussion/Discussion";
 import "./App.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div className="App">
+      <ToastContainer />
       <Discussion />
     </div>
   );
