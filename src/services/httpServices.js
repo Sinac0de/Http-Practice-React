4cacdd40c017b90681ab49887a252a52ac6ec352
Sina Moradian <sinacsharp@gmail.com>
2023-03-29 04:48:42 +0330

    Add axios interceptors

--- src/services/httpServices.js
+++ src/services/httpServices.js
@@ -2,6 +2,30 @@ import axios from "axios";
 
 axios.defaults.baseURL = "http://localhost:3001";
 
+axios.interceptors.request(
+  (request) => {
+    console.log(request);
+    //edit request ...
+    return request;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject();
+  }
+);
+
+axios.interceptors.response(
+  (response) => {
+    console.log(response);
+    //edit response ...
+    return response;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject();
+  }
+);
+
 const http = {
   get: axios.get,
   put: axios.put,
