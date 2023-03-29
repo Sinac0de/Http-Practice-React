1b1e6dc9bf48d5e13b1c1d2571be235fa257f94a
Sina Moradian <sinacsharp@gmail.com>
2023-03-29 05:17:48 +0330

    Add headers to the post operation

--- src/services/addNewCommentService.js
+++ src/services/addNewCommentService.js
@@ -1,5 +1,15 @@
 import http from "./httpServices";
 
-export function addNewComment(comment) {
-  return http.post("/comments", comment);
+export function addNewComment(data) {
+  const token = "SECURE TOKEN";
+  const header = {
+    headers: {
+      Authorization: token,
+    },
+  };
+  return http.post("/comments", data, header);
 }
+
+//for put:
+//for example we wanna change number 1 to the data
+//http.put("/comments/1",data)
