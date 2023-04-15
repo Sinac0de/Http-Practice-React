6ffac75fb3ce4385382b6d6a21e32837ce1497c5
Sina Moradian <sinacsharp@gmail.com>
2023-04-15 15:19:42 +0330

    Add Layout & routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import Discussion from "./container/Discussion/Discussion";
-import "./App.css";
+import Layout from "./layout/Layout";
 import { ToastContainer } from "react-toastify";
+import { Route, Switch } from "react-router-dom";
+import routes from "./routes";
 import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <ToastContainer />
-      <Discussion />
+      <Layout>
+        {/* check for urls with switch */}
+        <Switch>
+          {routes.map((route) => (
+            <Route {...route} />
+          ))}
+        </Switch>
+      </Layout>
     </div>
   );
 }
