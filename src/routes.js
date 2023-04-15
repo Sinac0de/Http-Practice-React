5a7e9bf00fe9f2a730779105c35e2417d252a1fc
Sina Moradian <sinacsharp@gmail.com>
2023-04-15 15:43:22 +0330

    Add dynamic route for a comment

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 import NewCommentPage from "./pages/NewCommentPage";
 import HomePage from "./pages/HomePage";
 import NotFoundPage from "./pages/NotFoundPage";
+import FullComment from "./components/FullComment/FullComment";
 
 const routes = [
+  { path: "/comments/:id", component: FullComment },
   { path: "/new-comment", component: NewCommentPage },
   { path: "/", component: HomePage, exact: true },
   { component: NotFoundPage },
