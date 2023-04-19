a58151701596e84d8a434cbcdd5e1903c1bb12ab
Sina Moradian <sinacsharp@gmail.com>
2023-04-19 14:54:16 +0330

    Change routes to the pages from components

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,11 @@
-import NewCommentPage from "./pages/NewCommentPage";
+import NewComment from "./pages/NewComment/NewComment";
+import FullComment from "./pages/FullComment/FullComment";
 import HomePage from "./pages/HomePage";
 import NotFoundPage from "./pages/NotFoundPage";
-import FullComment from "./components/FullComment/FullComment";
 
 const routes = [
   { path: "/comments/:id", component: FullComment },
-  { path: "/new-comment", component: NewCommentPage },
+  { path: "/new-comment", component: NewComment },
   { path: "/", component: HomePage, exact: true },
   { component: NotFoundPage },
   //we could also use path="*" for Not-Found
